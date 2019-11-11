09a84a28a9a87aa997c0dca614b6532f3b0e7a03
bewing09 <56164335+bewing09@users.noreply.github.com>
2019-11-11 10:50:43 -0500

    Update hello functions.ino

--- hellofunctions/hello functions.ino
+++ hellofunctions/hello functions.ino
@@ -13,28 +13,28 @@ void setup()
 
 void loop()
 {
-	servo();
-	print();
+	servo();// add servo function to void loop
+	print();// add print function to void loop
 }
 
 int getdistance()
 {
 	digitalWrite(trigpin, LOW);
-	delayMicroseconds(5);
+	delayMicroseconds(5); // 5 microseconds delay
 	digitalWrite(trigpin, HIGH);
-	delayMicroseconds(10);
+	delayMicroseconds(10); // 10 microsecond delay
 	digitalWrite(trigpin, LOW);
 	pinMode(echopin, INPUT);
 	duration = pulseIn(echopin, HIGH);
 	cm = (duration/2) / 29.1;
 	inches = (duration/2) / 74;
-	return inches;
+	return inches; // gives the amount of inches back
 }
 
 void servo()
 {
 	inches = getdistance();
-	if (inches < 10)
+	if (inches < 10)//
 	{
 		myservo.write(90);
 	}
@@ -46,8 +46,8 @@ void servo()
 
 void print()
 {
-	Serial.print(inches);
-	Serial.print("in, ");
+	Serial.print(inches);//put the interval inches in the serial monitor.
+	Serial.print("in");//
 	Serial.print(cm);
 	Serial.print("cm");
 	Serial.println();
